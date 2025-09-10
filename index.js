b58b3d1c972fccd8aef2f92555991dbd95e43590
TaranChase <chase.taran@gmail.com>
2025-09-11 00:07:31 +0300

    Using Conditionals to return back expected values.

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400) {
+    return "This one is on me!"
+  }
+
+  if(ride > 2500) {
+    return "No can do."
+  }
+
+
+  if(ride >= 400 && ride <= 2000) {
+    return "That will be twenty bucks."
+  }
+
+  if(ride >= 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+
+
 }
 
-function ternaryCheckCity(){
+console.log(scuberGreetingForFeet(400));
+
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
+
 }
 
-function switchOnCharmFromTip(){
+console.log(ternaryCheckCity("NYC"));
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+
+    case 'not as generous':
+      return "Thank you."
+
+    default:
+      return "Bye."
+  }
 }
+
+console.log(switchOnCharmFromTip())
